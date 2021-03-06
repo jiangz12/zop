31e0e7dfa358b07691064ab52949ec9bc5542ea8
Ryan <jiangz12@hotmail.com>
2021-03-06 14:20:06 +0800

    test sub commit

--- gitz.py
+++ gitz.py
@@ -58,6 +58,7 @@ def g_commit(comment):
     comment = "'" + comment + "'"
     for subdir in os.listdir(SUB_PATH):
         # check submodule .git file exists or not
+        print(os.system(DIFF))
         if os.path.exists(SUB_PATH + subdir + DOT_GIT) and os.system(DIFF) == 1:
             # excute submodule comit
             os.chdir(SUB_PATH + subdir)
