37ecb63e19860c06f3afee3e78cbd83c5a8f644e
Ryan <jiangz12@hotmail.com>
2021-03-06 14:54:37 +0800

    test multi sub update

--- gitz.py
+++ gitz.py
@@ -63,6 +63,7 @@ def g_commit(comment):
         if os.path.exists(SUB_PATH + subdir + DOT_GIT):
             # excute submodule comit
             os.chdir(SUB_PATH + subdir)
+            print(os.system(DIFF))
             if os.system(DIFF) == 1:
                 script(GIT_ADD_ALL)
                 script(GIT_COMMIT + comment)
